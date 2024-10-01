da317a291f61a991aa5a1329cedcfd8047cf5384
Buuhan4 <danivivasgames@gmail.com>
2024-10-01 20:00:00 +0200

    ejercicios

---
+++ ejercicio26.js
@@ -0,0 +1,10 @@
+class Libro {
+    constructor(titulo, autor, anio) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anio = anio;
+    }
+
+    console.console.log();
+    
+}
